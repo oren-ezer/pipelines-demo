60286bd5e8c4d8cfe6794cfd0bb2bc1636b4011f
orene <orene@jfrog.com>
2020-01-01 14:36:42 +0200

    creating the repo

---
+++ src/main/java/com/jfrog/pipelinesdemo/PipelinesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jfrog.pipelinesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfrog/pipelinesdemo/PipelinesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfrog.pipelinesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
